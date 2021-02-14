6e8989f60b2630507347e4623abe0048f5200acd
Sam-Berger <sam.d.berger@gmail.com>
2021-02-14 15:15:39 -0600

    finished part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg';
+  image3 = 'https://images.freeimages.com/images/large-previews/10f/autumn-1-1382513.jpg';
 
   constructor() { }
 
